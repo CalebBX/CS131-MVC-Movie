c785d33564ed445c2581b6b8c868b4e475311046
Caleb M Breaux <814196499@wwcc.edu>
2019-09-24 10:41:01 -0700

    First day additions

---
+++ MovieApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MovieApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+        public string Welcome(string name, int ID)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, your Id is: {ID}");
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+    }
+}
