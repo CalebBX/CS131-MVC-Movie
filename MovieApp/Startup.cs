70a4980e6782afed16c6c251e9478b353a164c33
Caleb M Breaux <814196499@wwcc.edu>
2019-09-26 10:58:08 -0700

    9-26-19 Added Movie Model and DB

--- MovieApp/Startup.cs
+++ MovieApp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MovieApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MovieApp
 {
@@ -33,6 +35,9 @@ namespace MovieApp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<MovieAppContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MovieAppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
